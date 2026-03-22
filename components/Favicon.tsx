8083966c456ef6cc94735689e07c97bf1b1d61a2
David Kamsiyochukwu <kamsidavid36@gmail.com>
2026-03-22 11:58:54 +0100

    updates

--- components/Favicon.tsx
+++ components/Favicon.tsx
@@ -1,17 +1,18 @@
 // app/components/Favicon.tsx
 "use client";
-import { SiWorldhealthorganization } from "react-icons/si";
 
 export function Favicon() {
   return (
     <link
       rel="icon"
       type="image/svg+xml"
-      href={`data:image/svg+xml,${encodeURIComponent(
-        `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="%2300B74A">
-          <path d="M12 0C5.372 0 0 5.372 0 12c0 6.627 5.372 12 12 12s12-5.373 12-12c0-6.628-5.372-12-12-12zm0 22c-5.522 0-10-4.478-10-10S6.478 2 12 2s10 4.478 10 10-4.478 10-10 10z"/>
-        </svg>`
-      )}`}
+      href={`data:image/svg+xml,${encodeURIComponent(`
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#00B74A">
+          <path d="M12 0C5.372 0 0 5.372 0 12s5.372 12 12 12 12-5.372 12-12S18.628 0 12 0zm0 22c-5.522 0-10-4.478-10-10S6.478 2 12 2s10 4.478 10 10-4.478 10-10 10z"/>
+          <path d="M12 6a6 6 0 100 12 6 6 0 000-12zm0 10a4 4 0 110-8 4 4 0 010 8z"/>
+          <path d="M12 10v4m-2-2h4" stroke="#00B74A" stroke-width="1.5" stroke-linecap="round"/>
+        </svg>
+      `)}`}
     />
   );
 }
